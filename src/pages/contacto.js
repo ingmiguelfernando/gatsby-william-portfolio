bf778329492512952b2a98ada6a02d153f71641f
ingmiguelfernando <ing.miguel.fernando@gmail.com>
2020-08-18 21:50:46 -0500

    update contacto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -16,7 +16,7 @@ const Contact = () => {
 
   const netlify = useNetlifyForm({
     name: "react-hook-form",
-    action: "/",
+    action: "/#servicios",
     honeypotName: "bot-field",
     onSuccess: (response, context) => {
       console.log("Successfully sent form data.")
@@ -28,94 +28,99 @@ const Contact = () => {
     <NetlifyFormProvider {...netlify}>
       <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)}>
         <Honeypot />
+
         <Layout>
           <SEO title="Contacto" description="Página de contacto" />
           <section className="contact-page">
-            <article className="contact-form">
+            {netlify.success && (
+              <p sx={{ variant: "alerts.success", p: 3 }}>
+                Tu mensaje ha sido enviado, Gracias por contactarnos!
+              </p>
+            )}
+            {netlify.error && (
+              <p sx={{ variant: "alerts.muted", p: 3 }}>
+                En este momento no es posible contactarnos, llámanos al (+57)
+                317 525 1469
+              </p>
+            )}
+            {!netlify.success && ( <article className="contact-form">
               <h3>Contacto</h3>
-              <form
-                name="contact"
-                method="POST"
-                data-netlify-honeypot="bot-field"
-                data-netlify="true"
-              >
-                <input type="hidden" name="form-name" value="contact" />
-                <div className="form-group">
-                  <input
-                    type="text"
-                    placeholder="nombre"
-                    name="name"
-                    className="form-control"
-                    ref={register({
-                      required: "ingresa tu nombre",
-                    })}
-                  />
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="nombre"
+                  name="name"
+                  className="form-control"
+                  ref={register({
+                    required: "ingresa tu nombre",
+                  })}
+                />
 
-                  {errors.name && (
-                    <div className="text-red-500 -mt-5">
-                      {" "}
-                      {errors.name.message}
-                    </div>
-                  )}
-
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="email"
-                    className="form-control"
-                    ref={register({
-                      pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: "email incorrecto",
-                      },
-                    })}
-                  />
-
-                  {errors.email && (
-                    <div className="text-red-500 -mt-5">
-                      {" "}
-                      {errors.email.message}
-                    </div>
-                  )}
-
-                  <input
-                    type="text"
-                    placeholder="teléfono o WhatsApp"
-                    name="phone"
-                    className="form-control"
-                    ref={register({
-                      required: "ingresa tu número telefónico o celular",
-                      pattern: {
-                        value: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im,
-                        message: "ingresa un número válido",
-                      },
-                    })}
-                  />
-
-                  {errors.phone && (
-                    <div className="text-red-500 -mt-5">
-                      {errors.phone.message}
-                    </div>
-                  )}
-
-                  <textarea
-                    name="message"
-                    rows="5"
-                    placeholder="mensaje"
-                    className="form-control"
-                    ref={register({
-                      required: "ingresa un mensaje",
-                    })}
-                  />
+                {errors.name && (
                   <div className="text-red-500 -mt-5">
-                    {errors.message && errors.message.message}
+                    {" "}
+                    {errors.name.message}
                   </div>
+                )}
+
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="form-control"
+                  ref={register({
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "email incorrecto",
+                    },
+                  })}
+                />
+
+                {errors.email && (
+                  <div className="text-red-500 -mt-5">
+                    {" "}
+                    {errors.email.message}
+                  </div>
+                )}
+
+                <input
+                  type="text"
+                  placeholder="teléfono o WhatsApp"
+                  name="phone"
+                  className="form-control"
+                  ref={register({
+                    required: "ingresa tu número telefónico o celular",
+                    pattern: {
+                      value: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im,
+                      message: "ingresa un número válido",
+                    },
+                  })}
+                />
+
+                {errors.phone && (
+                  <div className="text-red-500 -mt-5">
+                    {errors.phone.message}
+                  </div>
+                )}
+
+                <textarea
+                  name="message"
+                  rows="5"
+                  placeholder="mensaje"
+                  className="form-control"
+                  ref={register({
+                    required: "ingresa un mensaje",
+                  })}
+                />
+                <div className="text-red-500 -mt-5">
+                  {errors.message && errors.message.message}
                 </div>
-                <button type="submit" className="submit-btn btn">
-                  enviar
-                </button>
-              </form>
-            </article>
+              </div>
+              <button type="submit" className="submit-btn btn">
+                enviar
+              </button>
+            </article>)}
           </section>
         </Layout>
       </NetlifyFormComponent>
